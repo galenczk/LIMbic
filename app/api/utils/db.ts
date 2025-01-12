a2b1aa32a763f819a8d367415fc5f6d263da624b
galenczk <galenczk@gmail.com>
2025-01-12 14:27:43 -0800

    setting up env

---
+++ app/api/utils/db.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc, setDoc, Timestamp } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+console.log(process.env.TEST);
+
+/*
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app)
+
+try {
+    const querySnapshot = await getDocs(collection(db, 'users'));
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+    });
+} catch (e) {
+    console.error('Error adding document: ', e);
+}
+  */  
