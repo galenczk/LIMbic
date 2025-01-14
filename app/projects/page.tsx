3458f079c1c3f225edc30f678b6d0901ad884c49
galenczk <galenczk@gmail.com>
2025-01-13 18:10:39 -0800

    Migrated server logic to Projects Page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,19 +1,32 @@
 import Link from 'next/link';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../api/utils/db';
 
 export default async function projectsPage() {
-    let response = await fetch('http://localhost:3000/api/projects');
+    const querySnapshot = await getDocs(collection(db, 'projects'));
 
-    const projects = await response.json();
+    const projects = querySnapshot.docs.map((project: any) => ({
+        number: project.data().number,
+        name: project.data().name,
+        client: project.data().client,
+        type: project.data().type,
+        numberSamples: project.data().numberSamples,
+        openedDate: project.data().openedDate,
+        tat: project.data().tat,
+        dueDate: project.data().dueDate,
+        value: project.data().value,
+        projectId: project.data().projectId,
+    }));
 
     // Look at how to render date when you start adding projects via a form on client
     // Need to add openedDate, dueDate, and value. Should go opened, tat, due, value
     return (
         <div>
+            8
             <Link href={'/'} className='bg-blue-400'>
                 Home
             </Link>
             <h2>Projects</h2>
-
             <div>
                 <table>
                     <thead>
@@ -26,7 +39,7 @@ export default async function projectsPage() {
                         </tr>
                     </thead>
                     <tbody>
-                        {projects.map((project: any, key: string) => (
+                        {projects.map((project: any, key: number) => (
                             <tr key={key}>
                                 <td>{project.number}</td>
                                 <td>{project.name}</td>
@@ -34,7 +47,6 @@ export default async function projectsPage() {
                                 <td>{project.type}</td>
                                 <td>{project.numberSamples}</td>
                                 <td>{project.tat}</td>
-                                
                             </tr>
                         ))}
                     </tbody>
